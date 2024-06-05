b14c0a2e5d221f1500c68381f5e4d899e8a9e893
zlatanoviccs <zlatanovichs6@gmail.com>
2024-06-05 23:07:32 +0200

    Added footer contact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,5 +3,9 @@ export default function Footer() {
   // 109 rem // 1744 px
   /*<footer>
     Abc premium
+    fitness@abcpremium.com
+    // br tel
+    // instagram - fitness_premium_abc
+    
     </footer>*/
 }
