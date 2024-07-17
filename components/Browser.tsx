ed5b1ff292d061468b1de619a7dec38bd3ab81d6
zlatanovics1 <office@zlatanovics.com>
2024-07-17 22:36:21 +0200

    Change Browser component UI

--- components/Browser.tsx
+++ components/Browser.tsx
@@ -3,14 +3,14 @@ import sup from "@/public/abcsup.png";
 import { BiCircle, BiLinkExternal, BiSearch } from "react-icons/bi";
 export default function Browser() {
   return (
-    <div className="cursor-pointer relative max-w-[42rem] scale-90 rounded-2xl group  border border-primary-light/50">
+    <div className="cursor-pointer relative max-w-[42rem] scale-90 rounded-2xl group  border border-primary-light/50 shadow-sm shadow-primary-dark">
       <div className="flex items-center gap-2 [&_*]:w-3 [&_*]:h-3 absolute top-3 left-5">
         <BiCircle className="bg-gray-500 rounded-full text-gray-500" />
         <BiCircle className="bg-gray-700 rounded-full text-gray-700" />
         <BiCircle className="bg-gray-800 rounded-full text-gray-800" />
       </div>
       <div className="flex items-center justify-center py-2">
-        <div className="border pl-3 pr-14  bg-gray-900 flex items-center gap-2 border-gray-400 text-gray-400 rounded-md">
+        <div className="border pl-3 pr-14  bg-gray-800/80 flex items-center gap-2 border-gray-400 text-gray-400 rounded-md">
           <BiSearch />
           <span>abc-supplements.com</span>
         </div>
