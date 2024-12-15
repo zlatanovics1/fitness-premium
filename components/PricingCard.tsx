161013dd2236bc14585397de8b8ee2789f76a975
zlatanovics1 <office@zlatanovics.com>
2024-12-15 15:19:34 +0100

    feat: decrease basic price to -40%

--- components/PricingCard.tsx
+++ components/PricingCard.tsx
@@ -11,6 +11,7 @@ export default function PricingCard({
   index: number;
 }) {
   const t = useTranslations("PricingPage");
+  const isBasic = feature.type === "basic";
   const cardStyle = `bg-zinc-950 ${
     isMain ? "border-primary-dark border-2 py-14" : "border border-zinc-700"
   }`;
@@ -26,6 +27,11 @@ export default function PricingCard({
     <div
       className={`relative rounded-3xl p-8 shadow-2xl ring-1 ring-gray-900/10 sm:p-10 ${cardStyle} lg:first-of-type:rounded-r-none lg:last-of-type:rounded-l-none`}
     >
+      {isBasic && (
+        <span className="absolute top-0 left-0 rounded-3xl rounded-bl-none px-4 py-1 bg-primary-dark">
+          40% OFF
+        </span>
+      )}
       {index === 1 && <br />}
 
       <h3 className="text-base font-semibold leading-7 text-primary-dark uppercase">
@@ -33,12 +39,22 @@ export default function PricingCard({
       </h3>
       <p className="mt-4 flex items-baseline gap-x-2">
         <span className={`${isMain && "gold"}`}>
-          <span className="text-5xl font-bold tracking-tight">
-            {feature.price}
-          </span>
+          {isBasic ? (
+            <>
+              <span className="mr-2 font-semibold line-through decoration-red-500 text-3xl">
+                {feature.price}
+              </span>
+              <br />
+              <span className="text-5xl font-bold tracking-tight">4200</span>
+            </>
+          ) : (
+            <span className="text-5xl font-bold tracking-tight">
+              {feature.price}
+            </span>
+          )}
           <span className="text-2xl ml-1 tracking-tight font-bold">RSD</span>
+          <span className="text-base"> / month</span>
         </span>
-        <span className="text-base">/month</span>
       </p>
       <p className="mt-6 text-base leading-7">
         {t(`${feature.type}.headline`)}
